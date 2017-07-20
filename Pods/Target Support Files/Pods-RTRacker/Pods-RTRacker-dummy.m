fef265ef55eb35ea98bbf1e1c3124679a50910d8
jakeTran42 <jake.tranehs@gmail.com>
2017-07-20 11:32:24 -0700

    Login Initial VC

---
+++ Pods/Target Support Files/Pods-RTRacker/Pods-RTRacker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RTRacker : NSObject
+@end
+@implementation PodsDummy_Pods_RTRacker
+@end
---
+++ RTRacker/Controllers/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  RTRacker
+//
+//  Created by Jake Tran on 7/20/17.
+//  Copyright © 2017 Jake Tran. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Additional SetUp after loading the view
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //Discpose of resource that can be recreated
+    }
+
+}
